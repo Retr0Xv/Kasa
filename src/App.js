ddfb06bbb7e78aa5949b5e0a8518369786ea903d
Retr0Xv <bijutheo@gmail.com>
2023-09-06 15:09:07 +0200

    Site avec scss (erreur)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './style/main.scss';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Error from "./pages/Error/Error";
